0dfbb878831b0e8c1565b27c73ae856f32252e69
mohamed <mezoattalla123@gmail.com>
2024-11-14 13:42:55 +0200

    ✨ feat(auth): Implement sign up view body and integrate it into sign up view

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 
+import 'widgets/sign_up_view_body.dart';
+
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
 
@@ -10,6 +12,7 @@ class SignUpView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context, title: 'حساب جديد'),
+      body: const SignUpViewBody(),
     );
   }
 }
---
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/widgets/custom_button.dart';
+import 'package:fruit_hub/core/widgets/custom_text_field.dart';
+import 'package:fruit_hub/generated/l10n.dart';
+
+import 'display_tems_and_condaitions.dart';
+import 'have_an_account_widget.dart';
+
+class SignUpViewBody extends StatelessWidget {
+  const SignUpViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 24,
+            ),
+            const CustomTextField(
+              hintText: 'الاسم كامل',
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hintText: S.of(context).email,
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hintText: S.of(context).password,
+              keyboardType: TextInputType.visiblePassword,
+              suffixIcon: const Icon(
+                Icons.remove_red_eye,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const TermsAndConditionsWidget(),
+            const SizedBox(
+              height: 32,
+            ),
+            CustomButton(title: 'إنشاء حساب جديد', onPressed: () {}),
+            const SizedBox(
+              height: 24,
+            ),
+            HaveAnAccountWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
