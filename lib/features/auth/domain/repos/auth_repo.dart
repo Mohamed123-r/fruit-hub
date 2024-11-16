74a06bf300b90031b7882aa02faa55ac5068a5c6
mohamed <mezoattalla123@gmail.com>
2024-11-16 15:06:34 +0200

    ✨ [FEAT](auth): Add auth repository and dartz dependency

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_hub/core/error/failures.dart';
+import 'package:fruit_hub/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      {required String email, required String password});
+}
