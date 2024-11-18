dee1da4fc102dc41fe7a7096898eb9f67fb36d1f
mohamed <mezoattalla123@gmail.com>
2024-11-18 14:45:09 +0200

    ✨ [FEAT](auth): Add sign up cubit and state 📦 [CHORE](deps): Add bloc and flutter_bloc packages

---
+++ lib/features/auth/presentation/manage/sign_up_cubit/sign_up_cubit.dart
@@ -0,0 +1,32 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruit_hub/features/auth/domain/entities/user_entity.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit(this.authRepo) : super(SignUpInitial());
+
+  final AuthRepo authRepo;
+
+  Future<void> createUserWithEmailAndPassword(
+      {required String email,
+      required String password,
+      required String name}) async {
+    emit(SignUpLoading());
+    final result = await authRepo.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+      name: name,
+    );
+    result.fold(
+      (failure) => emit(SignUpFailure(
+        massage: failure.message, // emit error
+      )),
+      (userEntity) => emit(SignUpSuccess(
+        userEntity: userEntity,
+      )),
+    );
+  }
+}
