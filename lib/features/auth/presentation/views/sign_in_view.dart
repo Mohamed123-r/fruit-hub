e29416d8e6dcb09285fd27db5ffbc3308c51054a
mohamed <mezoattalla123@gmail.com>
2024-11-19 16:12:09 +0200

    ✨ feat(auth/presentation): Add sign in view UI and bloc provider

---
+++ lib/features/auth/presentation/views/sign_in_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/helper_funcation/get_it.dart';
+import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruit_hub/generated/l10n.dart';
+import '../../domain/repos/auth_repo.dart';
+import '../manage/sign_in_cubit/sign_in_cubit.dart';
+import 'widgets/sign_in_view_body_bloc_consumer.dart';
+
+class LogInView extends StatelessWidget {
+  const LogInView({super.key});
+
+  static const routeName = '/log-in-view';
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => SignInCubit(
+        getIt<AuthRepo>(),
+      ),
+      child: Scaffold(
+        appBar: buildAppBar(
+          context,
+          title: S.of(context).log_in_text,
+        ),
+        body: SignInViewBodyBlocConsumer(),
+      ),
+    );
+  }
+}
