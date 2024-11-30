37e02f0f7648ceb97b73ec1e803beca7072c866d
mohamed <mezoattalla123@gmail.com>
2024-11-30 17:07:36 +0200

    ✨ [FEAT](widgets/custom_bottom_navigation_bar): Implements custom bottom navigation bar widget.

--- lib/core/widgets/custom_bottom_navigation_bar.dart
+++ lib/core/widgets/custom_bottom_navigation_bar.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 
-class CustomBottomNavigationBar extends StatelessWidget {
+import '../../features/home/domain/entities/bottom_navigation_bar_entity.dart';
+import '../../features/home/presentation/views/widgets/bottom_navigation_bar_item.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({super.key});
+
+  @override
+  State<CustomBottomNavigationBar> createState() =>
+      _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  int selectIndex = 3;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-
-    );
+        height: 60,
+        width: double.infinity,
+        decoration: ShapeDecoration(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          color: Colors.white,
+          shadows: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Row(
+            children: bottomNavigationBarItems.asMap().entries.map((e) {
+          var index = e.key;
+          var item = e.value;
+          return Expanded(
+            flex: index == selectIndex ? 3 : 2,
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  selectIndex = index;
+                });
+              },
+              child: BottomNavigationBarItems(
+                isActive: selectIndex == index,
+                bottomNavigationBarEntity: item,
+              ),
+            ),
+          );
+        }).toList()));
   }
 }
