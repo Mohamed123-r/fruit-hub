f75553bc43076544f9697a9204f7d3b77b42aa45
mohamed <mezoattalla123@gmail.com>
2024-12-21 14:45:24 +0200

    ✨ feat(entities): Add product and review entities

---
+++ lib/core/entities/product_entity.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:fruit_hub/core/entities/review_entity.dart';
+
+
+class ProductEntity {
+  final String name;
+  final String description;
+  final num price;
+  final File image;
+  final bool isFeatured;
+  final String code;
+  String? urlImage;
+  final int expirationMonths;
+  final bool isOrganic;
+  final int numberOfCalories;
+  final int unitAmount;
+  final num aveRating;
+  final num ratingCount;
+  final List<ReviewEntity> reviews ;
+
+
+  ProductEntity({
+    required this.name,
+    required this.isFeatured,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.code,
+    this.urlImage,
+    required this.expirationMonths,
+    required this.numberOfCalories,
+    required this.unitAmount,
+    this.aveRating = 0,
+    this.ratingCount = 0,
+    this.isOrganic = false,
+    required this.reviews ,
+  });
+}
