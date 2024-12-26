e2d8520ea46c755b37d27d87fd0eabe5b2db5b5e
mohamed <mezoattalla123@gmail.com>
2024-12-26 20:11:49 +0200

    ✨ feat(widgets/custom_app_bar): Adds optional leading icon to AppBar
    ✨ feat(views/product_view): Integrates ProductCubit and adds leading icon to AppBar

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -3,23 +3,27 @@ import 'package:flutter/material.dart';
 import '../utils/app_text_styles.dart';
 import 'notification_widget.dart';
 
-AppBar buildAppBar({
-  required BuildContext context,
-  required String title,
-}) {
+AppBar buildAppBar(
+    {required BuildContext context,
+    required String title,
+    bool showLeading = false}) {
   return AppBar(
     title: Text(title, style: TextStyles.bodyLargeBold),
     centerTitle: true,
     backgroundColor: Colors.white,
+    surfaceTintColor: Colors.white,
     elevation: 0,
-    leading: IconButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: const Icon(
-          Icons.arrow_back_ios_new_outlined,
-          size: 18,
-        )),
+    leading: Visibility(
+      visible: showLeading,
+      child: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new_outlined,
+            size: 18,
+          )),
+    ),
     actions: const [
       Padding(
         padding: EdgeInsets.only(right: 16.0, left: 16.0),
--- lib/features/main/presentation/views/product_view.dart
+++ lib/features/main/presentation/views/product_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/cubits/product_cubit/product_cubit.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruit_hub/features/main/presentation/views/widgets/product_view_body.dart';
 
+import '../../../../core/helper_funcation/get_it.dart';
+import '../../../../core/repos/product_repo.dart';
 import '../../../../generated/l10n.dart';
 
 class ProductView extends StatelessWidget {
@@ -9,12 +13,18 @@ class ProductView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(
-        context: context,
-        title: S.of(context).bottom_navigation_products,
+    return BlocProvider(
+      create: (context) => ProductCubit(
+        productRepo: getIt.get<ProductRepo>(),
       ),
-      body: ProductViewBody(),
+      child: Scaffold(
+        appBar: buildAppBar(
+          context: context,
+          showLeading: true,
+          title: S.of(context).bottom_navigation_products,
+        ),
+        body: ProductViewBody(),
+      ),
     );
   }
 }
