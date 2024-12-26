d5fc94f25b2baf383b273558107198443e7c99f6
mohamed <mezoattalla123@gmail.com>
2024-12-26 17:14:45 +0200

    ✨ (UI): Adds placeholder image for missing product images in FruitItem widget.

--- lib/core/widgets/fruit_item.dart
+++ lib/core/widgets/fruit_item.dart
@@ -20,22 +20,21 @@ class FruitItem extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Stack(
           children: [
-            Positioned(
-              top: 0,
-              left: !isArabic() ? 0 : null,
-              right: isArabic() ? 0 : null,
-              child: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.favorite_border_outlined),
-              ),
-            ),
             Column(
               children: [
                 const SizedBox(height: 12),
                 Flexible(
-                  child: Image.network(
-                    product.image.path,
-                  ),
+                  child: product.image.path.isEmpty
+                      ? Container(
+                          height: 100,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.grey.shade200),
+                        )
+                      : Image.network(
+                          product.image.path,
+                        ),
                 ),
                 const SizedBox(height: 12),
                 ListTile(
@@ -68,6 +67,15 @@ class FruitItem extends StatelessWidget {
                   ),
                 )
               ],
+            ),
+            Positioned(
+              top: 0,
+              left: !isArabic() ? 0 : null,
+              right: isArabic() ? 0 : null,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border_outlined),
+              ),
             ),
           ],
         ),
