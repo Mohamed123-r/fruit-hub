89dd9f22df1ad81a8d2c4f09ef4302ba83ad86c4
mohamed <mezoattalla123@gmail.com>
2024-12-29 16:40:19 +0200

    ✨ (main): Integrates CartCubit and conditionally renders cart elements based on cart items.

--- lib/features/main/presentation/views/main_view.dart
+++ lib/features/main/presentation/views/main_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/widgets/custom_bottom_navigation_bar.dart';
+import 'package:fruit_hub/features/main/presentation/manage/cubits/cart_cubit/cart_cubit.dart';
 import 'package:fruit_hub/features/main/presentation/views/home_view.dart';
 import 'package:fruit_hub/features/main/presentation/views/product_view.dart';
 
@@ -20,22 +22,25 @@ class _MainViewState extends State<MainView> {
   @override
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: CustomBottomNavigationBar(
-        onItemTapped: (int value) {
-          setState(() {
-            currentIndex = value;
-          });
-        },
-      ),
-      body: SafeArea(
-        child: IndexedStack(
-          index: currentIndex,
-          children: [
-            HomeView(),
-            ProductView(),
-            CartView(),
-          ],
+    return BlocProvider(
+      create: (context) => CartCubit(),
+      child: Scaffold(
+        bottomNavigationBar: CustomBottomNavigationBar(
+          onItemTapped: (int value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+        ),
+        body: SafeArea(
+          child: IndexedStack(
+            index: currentIndex,
+            children: [
+              HomeView(),
+              ProductView(),
+              CartView(),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/main/presentation/views/widgets/cart_view_body.dart
+++ lib/features/main/presentation/views/widgets/cart_view_body.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/services/bloc_opesever.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/utils/app_text_styles.dart';
 import 'package:fruit_hub/core/widgets/custom_button.dart';
 
+import '../../manage/cubits/cart_cubit/cart_cubit.dart';
 import 'cart_items_list.dart';
-
 
 class CartViewBody extends StatelessWidget {
   const CartViewBody({super.key});
@@ -16,35 +18,45 @@ class CartViewBody extends StatelessWidget {
         CustomScrollView(
           slivers: [
             SliverToBoxAdapter(
-              child: Container(
-                height: 40,
-                width: double.infinity,
-                color: Color(0xffEBF9F1),
-                child: Center(
-                  child: Text(
-                    "لديك 3 منتجات في سله التسوق",
-                    style: TextStyles.bodySmallRegular.copyWith(
-                      color: AppColors.greenColor,
+              child: context.read<CartCubit>().cartEntity.cartItems.isEmpty
+                  ? SizedBox()
+                  : Container(
+                      height: 40,
+                      width: double.infinity,
+                      color: Color(0xffEBF9F1),
+                      child: Center(
+                        child: Text(
+                          "لديك 3 منتجات في سله التسوق",
+                          style: TextStyles.bodySmallRegular.copyWith(
+                            color: AppColors.greenColor,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
             ),
             SliverToBoxAdapter(
               child: SizedBox(
                 height: 24,
               ),
             ),
-            CartItemsList(cartListEntity: [],),
+            CartItemsList(
+              cartListEntity: [],
+            ),
           ],
         ),
         Padding(
-            padding: EdgeInsets.only(
-              top: MediaQuery.of(context).size.height * 0.72,
-              right: 16,
-              left: 16,
-            ),
-            child: CustomButton(title: "الدفع  120جنيه", onPressed: () {}))
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * 0.72,
+            right: 16,
+            left: 16,
+          ),
+          child: context.read<CartCubit>().cartEntity.cartItems.isEmpty
+              ? SizedBox()
+              : CustomButton(
+                  title: "الدفع  120جنيه",
+                  onPressed: () {},
+                ),
+        ),
       ],
     );
   }
